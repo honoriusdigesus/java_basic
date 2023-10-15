41e8baeb0678396fef35d4b57fae0a05bc64412f
Honorius di Gesus <honoriusdigesus@gmail.com>
2023-10-15 16:58:26 -0500

    Iniciamos el estudio de java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Variables
+        int i = 0; //Variable tipo entero
+        long l = 1000000; //Variable tipo entero
+        double d =1.55;
+        String s = "Cadena de texto";
+        boolean b  = true;
+
+        System.out.println("Las variables son: " + i);
+
+}
+}
